a166d636af023ccafe707a9835e498506c125e05
Surajshivam-123 <sj919810@gmail.com>
2025-08-22 17:13:39 +0530

    commit

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,13 +1,22 @@
-import AppRoutes from './routes/Route'
+import { useState } from "react";
+import AppRoutes from "./routes/Route";
+import { useEffect } from "react";
+import { data } from "react-router-dom";
 
 function App() {
-  
-
+  const [status, setStatus] = useState("Loading...");
+  useEffect(() => {
+    const API = import.meta.env.VITE_API_URL;
+    fetch(`${API}/campus-sphere`)
+      .then((res) => res.text)
+      .then((data) => setStatus(data))
+      .catch(() => setStatus("Something went wrong"));
+  },[]);
   return (
     <>
-      <AppRoutes/>
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
