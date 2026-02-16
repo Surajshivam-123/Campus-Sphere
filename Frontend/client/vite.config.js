cd02334cb4ecdd2ea74b47b52a79090ce219bb09
Surajshivam-123 <sj919810@gmail.com>
2026-02-17 00:32:45 +0530

    deployment

--- Frontend/client/vite.config.js
+++ Frontend/client/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/Campus-Sphere/'
   // server:{
   //   proxy:{
   //     '/api': 'http://localhost:3000'
